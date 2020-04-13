e58331c6a60895691fb6e005b38387b49a3f723c
jaden-song <mingup@ssw0rd>
2020-04-13 10:58:49 +0900

    0.0.5 nonstop 배포 적용

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ec2-user/app/nonstop/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+# 기존 travis 에서 nonstop 으로 변경
+# /home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
